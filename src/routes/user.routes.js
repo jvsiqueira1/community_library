70942daf661886babdef6bc683104a4adf66754d
João Vítor de Siqueira Campos <joaovitorsc@gmail.com>
2025-06-26 15:50:59 -0400

    Feat: dynamic UPDATE query

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express'
 import userController from '../controller/user.controllers.js'
+import { validate, validateUserId } from '../middlewares/validation.middlewares.js'
+import { userSchema } from '../schema/user.schema.js'
 
 const router = Router()
 
 
-router.post('/users', userController.createUserController)
+router.post('/users', validate(userSchema), userController.createUserController)
+router.get('/users', userController.findAllUserController)
+router.get('/users/:id', validateUserId, userController.findUserByIdController)
+router.patch('/users/:id', validateUserId, userController.updateUserController)
+router.delete('/users/:id', validateUserId, userController.deleteUserController)
 
 export default router
--- src/service/user.services.js
+++ src/service/user.services.js
@@ -14,4 +14,32 @@ async function createUserService(newUser) {
     return user
 }
 
-export default { createUserService }
+async function findAllUsersService() {
+    const users = await userRepository.findAllUserRepository()
+    return users
+}
+
+async function findUserByIdService(id) {
+    const user = await userRepository.findUserByIdRepository(id)
+    if (!user) throw new Error("User not found")
+    return user
+}
+
+async function updateUserService(newUser, userId) {
+    const user = await userRepository.findUserByIdRepository(userId)
+    if (!user) throw new Error("User not found")
+    if(newUser.password) {
+        newUser.password = await bcrypt.hash(newUser.password, 10)
+    }
+    const userUpdated = userRepository.updateUserRepository(userId, newUser)
+    return userUpdated
+}
+
+async function deleteUserService(userId) {
+    const user = await userRepository.findUserByIdRepository(userId)
+    if (!user) throw new Error("User not found")
+    const { message } = await userRepository.deleteUserRepository(userId)
+    return message
+}
+
+export default { createUserService, findAllUsersService, findUserByIdService, updateUserService, deleteUserService }
